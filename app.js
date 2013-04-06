cab967a5c5f9de5ee11811a97bd0e1d8bed6102f
Aliev Ramil <bezdonas@gmail.com>
2013-04-07 00:25:07 +0400

    Arrange matchs
    Our pong app can sort all this sockets and search for rivals.
    The problem is that game itself is not working yet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-// Server
+/*
+ *	Server
+ */
 var express = require('express'),
-    app = express.createServer(),
+    app = express(),
     socket = require('socket.io');
 
 app.configure(function () {
@@ -12,58 +14,120 @@ var server = app.listen(81);
 var io = socket.listen(server);
 
 
-// Sockets
-io.sockets.on('connection', function (socket) {
-    console.log("connnect");
 
-    // Client sent us position
-	socket.on('positions', function (data) {
-		Pong.positions = data;
-	});
+/*
+ *  Game backend object
+ */
+var Pong = {
+	// here we will keep sessions as socketId: socket
+	sessions: {
+		hanging: {}, // here will be sessions not in match
+		fighting: {}, // and this guys are already having fun
+		pairs: {} // and here we'll probably keep matching pairs
+	},
 
+	// Game backend application
+	app: (function(){
+		var me = {}; // returned object
 
-	// Client sent us actions
-	socket.on('left', function() {
-		Pong.positions.pad_1.left -= 10;
-		if (Pong.positions.pad_1.left < 0) {
-			Pong.positions.pad_1.left = 0;
-		}
-	});
+		// Initialization currently runs connectSocket()
+		me.init = function() {
+			me.connectSocket();
+		};
 
-	socket.on('right', function() {
-		Pong.positions.pad_1.left += 10;
-		if (Pong.positions.pad_1.left > 300) {
-			Pong.positions.pad_1.left = 300;
-		}
-	});
+		// This one opens socket connection, saves socket shortcuts in sessions object
+		// and calls arrangeMatch if there are more than two connections
+		me.connectSocket = function() {
+			io.sockets.on('connection', function (socket) {
 
-	socket.on('start', function() {
-	});
+				Pong.sessions.hanging[socket.id] = socket; // add the session data to sessions object
 
+				me.searchForMatches();
 
-	// Resend positions once awhile
-	setInterval(function(){
-		socket.emit('positions', Pong.positions);
-	}, 20);
+				socket.on('disconnect', function () {
+					// remove the session from the object whether it is in fight or no
+					delete Pong.sessions.fighting[socket.id];
+					delete Pong.sessions.hanging[socket.id];
+					me.searchForMatches();
+				});
 
+			});
+		};
 
-    socket.on('disconnect', function (socket) {
-        console.log("disconnect");
-    });
-});
+		// Looks if we have hanging pair for match now
+		// This one is circular
+		me.searchForMatches = function() {
+			var arr = []; // here we'll keep array of hanging id's
 
-// Game
-Pong = {
-	positions: {
-		ball: {
-			left: 0,
-			top: 0
-		},
-		pad_1: {
-			left: 0
-		},
-		pad_2: {
-			left: 0
-		}
-	}
+			// push ids into array
+			for (key in Pong.sessions.hanging) {
+				arr.push(key);
+			}
+
+			// calls arrangeMatch() if there is more than one hanging player
+			if (arr.length === 1) {
+				// one session, tell him to wait
+				Pong.sessions.hanging[arr[0]].emit('log', 'Wait for opponent');
+				return false;
+			} else if (arr.length === 2) {
+				// two sessions, let them fight each other
+				me.arrangeMatch(arr[0], arr[1]);
+				return false;
+			} else if (arr.length > 2) {
+				// more than two, arrange fight between two and do this function again
+				me.arrangeMatch(arr[0], arr[1]);
+				me.searchForMatches();
+			}
+
+		};
+
+		// arranges match. id1 and id2 are rival's session id (kept in Pong.sessions object)
+		me.arrangeMatch = function(id1, id2) {
+
+			var ss = Pong.sessions; // shortcut for pong sessions
+
+			// Transfer id's from hanging into fighting ones
+			ss.fighting[id1] = ss.hanging[id1];
+			ss.fighting[id2] = ss.hanging[id2];
+			delete ss.hanging[id1];
+			delete ss.hanging[id2];
+
+			// Shortcuts for fighting sockets
+			var socket1 = ss.fighting[id1],
+				socket2 = ss.fighting[id2],
+				// that is for keeping keymaps
+				keyMap1 = {},
+				keyMap2 = {},
+				// readiness
+				ready1: false,
+				ready2: false,
+				// provide broadcast for this to id's
+				broadcast = function(name, data) {
+					socket1.emit(name, data);
+					socket2.emit(name, data);
+				},
+				// key map reader
+				keyMapReader = function() {
+				};
+
+			broadcast('log', 'Press spacebar when you\'ll be ready to play');
+			
+			socket1.on('keyInfo', function(data) {
+				keyMap1 = data;
+				keyMapReader();
+			});
+
+			socket2.on('keyInfo', function(data) {
+				keyMap2 = data;
+				keyMapReader();
+			});
+
+		};
+
+		return me;
+	}())
 };
+
+
+// Kick-off
+Pong.app.init();
--- js/pong-game.js
+++ js/pong-game.js
@@ -1,115 +1,75 @@
 /*
+ *	Front-end Pong game app
  *	@author Aliev Ramil
  *	@email bezdonas@gmail.com
  */
 
-
-// Game object
-var Pong = {};
-
-
-// Game parameters, some of them need to be also edited in styl(css)
-Pong.params = {
-	wrap: {
-		width: 400,
-		height: 600
-	},
-	ball: {
-		width: 10,
-		height: 10
-	},
-	pad: {
-		width: 100,
-		height: 10
-	},
-	frameRate: 30,
-	keys: {
+// Game frontend object
+var Pong = {
+	// key
+	keyMap: {
 		37: 'left', // left arrow
 		39: 'right', // right arrow
 		32: 'start' // space
-	}
-};
-
-
-// Game objects default position (will be resent by server while playing)
-Pong.positions = {
-	ball: {
-		left: Pong.params.wrap.width / 2 - Pong.params.ball.width / 2,
-		top: Pong.params.wrap.height / 2 - Pong.params.ball.height / 2
 	},
-	pad_1: {
-		left: Pong.params.wrap.width / 2 - Pong.params.pad.width / 2
+
+	// here we'll keep info about key status (1 - pressed, 0 - not pressed)
+	keyInfo: {
+		'left': 0,
+		'right': 0,
+		'start': 0
 	},
-	pad_2: {
-		left: Pong.params.wrap.width / 2 - Pong.params.pad.width / 2
-	}
+
+	// Game frontend application
+	app: (function(){
+		var me = {}; // returned object
+
+		// jQuery objects
+		var doc = $(document),
+			wrap = $('#pong-game'),
+			ball = $('#pong-game_ball'),
+			pad_1 = $('#pong-game_pad-1'),
+			pad_2 = $('#pong-game_pad-2'),
+			log = $('#log');
+
+		// Other vars
+		var socket;
+
+		me.init = function() {
+			me.socketStart();
+			me.keyEvents();
+		};
+
+		me.socketStart = function() {
+			socket = io.connect();
+			socket.on('log', function(data) {
+				log.append(data, '<br/>');
+			});
+			socket.on('process', function(data) {
+				me.process(data);
+			});
+		};
+
+		me.keyEvents = function() {
+			doc.on('keydown keyup', function(event) {
+				var key = '' + event.which, // key code in string
+					keyMap = Pong.keyMap, // shortcut
+					keyInfo = Pong.keyInfo; // shortcut
+				// this provides actual information in keyMap and sending it to socket onchange
+				if (keyMap[key] !== undefined) {
+					keyInfo[keyMap[key]] = event.type === 'keydown' ? 1 : 0;
+					socket.emit('keyInfo', keyInfo);
+					return false
+				}
+			});
+		};
+
+		me.process = function(data) {
+		};
+
+		return me;
+	}())
 };
-
-
-/*
-	This module connects to socket and sends starting positions to it.
-	It also registers keypresses and sends them to socket as well.
-	Once a while it applies position of pads and ball from Pong.positions object.
-	That's all: game logic runs on the server.
-*/
-Pong.app = (function(){
-	var me = {}; // returned object
-
-	// jQuery objects
-	var wrap = $('#pong-game'),
-		ball = $('#pong-game_ball'),
-		pad_1 = $('#pong-game_pad-1'),
-		pad_2 = $('#pong-game_pad-2');
-
-	// Other vars
-	var intervalWrap, socket,
-		interval = 1000 / Pong.params.frameRate,
-		keys = Pong.params.keys;
-
-	me.init = function() {
-		me.socketStart();
-		me.registerKeypresses();
-		me.reflow();
-	};
-
-	me.reflow = function() {
-		// this interval applies css settings from Pong.positions
-		// to jQuery objects from this module
-		intervalWrap = setInterval(function() {
-
-			ball.css(Pong.positions.ball);
-			pad_1.css(Pong.positions.pad_1);
-			pad_2.css(Pong.positions.pad_2);
-
-		}, interval);
-	};
-
-	me.socketStart = function() {
-		// connect
-		socket = io.connect();
-
-		// send server start positions
-		socket.emit('positions', Pong.positions);
-
-		// this event sends us new positions from server
-		socket.on('positions', function (data) {
-			Pong.positions = data;
-		});	
-	};
-
-	me.registerKeypresses = function() {
-		$(document).on('keydown', function(e) {
-			var key = e.keyCode;
-			if (keys[key]) {
-				socket.emit(keys[key]);
-				return false;
-			}
-
-		});
-	};
-
-	return me;
-})();
 
 
 // Kick-off
