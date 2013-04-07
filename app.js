1c70c424591caf8c7bcc0defca97e1e29aaf0fce
Aliev Ramil <bezdonas@gmail.com>
2013-04-07 17:19:15 +0400

    Nothing special
    Comments, changed port to 80, re-arranging arrangeMatch function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.configure(function () {
     app.use(express.static(__dirname + '/'));
 });
 
-var server = app.listen(81);
+var server = app.listen(80);
 
 var io = socket.listen(server);
 
@@ -22,9 +22,14 @@ var Pong = {
 	// here we will keep sessions as socketId: socket
 	sessions: {
 		hanging: {}, // here will be sessions not in match
-		fighting: {}, // and this guys are already having fun
-		pairs: {} // and here we'll probably keep matching pairs
+		fighting: {} // and this guys are already having fun
 	},
+
+	// object for matches
+	matches: {},
+
+	// counter of matches (for creating id's for this.matches)
+	matchCounter: 0,
 
 	// Game backend application
 	app: (function(){
@@ -36,7 +41,7 @@ var Pong = {
 		};
 
 		// This one opens socket connection, saves socket shortcuts in sessions object
-		// and calls arrangeMatch if there are more than two connections
+		// and calls searchForMatches()
 		me.connectSocket = function() {
 			io.sockets.on('connection', function (socket) {
 
@@ -55,7 +60,8 @@ var Pong = {
 		};
 
 		// Looks if we have hanging pair for match now
-		// This one is circular
+		// This one is circular, works until cleares out current
+		// hanging sessions
 		me.searchForMatches = function() {
 			var arr = []; // here we'll keep array of hanging id's
 
@@ -82,45 +88,19 @@ var Pong = {
 		};
 
 		// arranges match. id1 and id2 are rival's session id (kept in Pong.sessions object)
+		// moves sockets for players from Pong.hanging to Pong.fighting and
+		// creates 
 		me.arrangeMatch = function(id1, id2) {
 
 			var ss = Pong.sessions; // shortcut for pong sessions
 
 			// Transfer id's from hanging into fighting ones
-			ss.fighting[id1] = ss.hanging[id1];
-			ss.fighting[id2] = ss.hanging[id2];
-			delete ss.hanging[id1];
-			delete ss.hanging[id2];
+			ss.fighting[players[1]] = ss.hanging[players[1]];
+			ss.fighting[players[2]] = ss.hanging[players[2]];
+			delete ss.hanging[players[1]];
+			delete ss.hanging[players[2]];
 
-			// Shortcuts for fighting sockets
-			var socket1 = ss.fighting[id1],
-				socket2 = ss.fighting[id2],
-				// that is for keeping keymaps
-				keyMap1 = {},
-				keyMap2 = {},
-				// readiness
-				ready1: false,
-				ready2: false,
-				// provide broadcast for this to id's
-				broadcast = function(name, data) {
-					socket1.emit(name, data);
-					socket2.emit(name, data);
-				},
-				// key map reader
-				keyMapReader = function() {
-				};
-
-			broadcast('log', 'Press spacebar when you\'ll be ready to play');
-			
-			socket1.on('keyInfo', function(data) {
-				keyMap1 = data;
-				keyMapReader();
-			});
-
-			socket2.on('keyInfo', function(data) {
-				keyMap2 = data;
-				keyMapReader();
-			});
+			// Create new match in this.matches object
 
 		};
 
